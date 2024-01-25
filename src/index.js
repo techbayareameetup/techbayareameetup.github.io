07390217f5b5195eea95e13fcb46257bd77a55df
quantusinc2 <admin@mailchamp.ai>
2024-01-24 16:54:09 -0800

    tag manager

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+	gtmId: 'GTM-MZDSB75R'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
 	<React.StrictMode>
